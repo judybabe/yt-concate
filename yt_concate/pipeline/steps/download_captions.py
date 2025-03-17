0fb8c0bfe154ae6519ef4dc776728b5fcad0455e
Judy Liao <judy1129888@gmail.com>
2025-03-17 14:25:08 +0800

    Fixed downloading videos, Finished editing videos and producing output video file.

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -3,6 +3,7 @@ import time
 import yt_dlp
 import webvtt
 
+
 from .step import Step
 from .step import StepException
 from yt_concate.settings import CAPTIONS_DIR
@@ -12,48 +13,32 @@ class DownloadCaptions(Step):
 
     def process(self, data, inputs, utils):
         start = time.time()
-        ydl_opts = {
-            'skip_download': True,  # 不下載影片，只下載字幕
-            'writesubtitles': False,  # 下載字幕
-            'writeautomaticsub': True,  # 下載自動字幕 (如果沒有人工字幕)
-            'subtitleslangs': ['en'],  # 選擇字幕語言 (例如 'en'、'zh-TW'、'ja' 等)
-            'subtitlesformat': 'vtt',  # 直接下載 VTT 格式
-            'outtmpl': os.path.join(CAPTIONS_DIR, '%(id)s.%(ext)s'),  # 儲存位置與檔名
-            'quiet': True,
-        }
-
         for yt in data:
             if utils.caption_file_exists(yt):
                 print('found existing caption file')
                 continue
-            else:
+
+            ydl_opts = {
+                'skip_download': True,  # 不下載影片，只下載字幕
+                'writeautomaticsub': True,
+                'subtitleslangs': ['en'],  # 選擇字幕語言 (例如 'en'、'zh-TW'、'ja' 等)
+                'outtmpl': os.path.join(CAPTIONS_DIR, '%(id)s.%(ext)s'),  # 儲存位置與檔名
+                'nooverwrites': True,
+            }
+
+            try:
                 video_id = yt.id
                 print(f"正在下載 {video_id} 的字幕...")
-            try:
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     ydl.download([yt.url])
-                self.convert_vtt_to_srt(video_id)
+                self.fix_youtube_vtt(video_id)
                 self.convert_srt_to_txt(video_id)
             except Exception as e:
                 print(f"無法下載 {video_id} 的字幕，錯誤: {e}")
         end = time.time()
         print('took', end - start, 'seconds')
+
         return data
-
-    def convert_vtt_to_srt(self, video_id):
-        vtt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.en.vtt")
-        srt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.srt")
-
-        if not os.path.exists(vtt_path):
-            return
-
-        try:
-            webvtt.read(vtt_path).save_as_srt(srt_path)
-            print(f"已產生 {video_id}.srt")
-            os.remove(vtt_path)
-        except Exception as e:
-            print(f"轉檔 {video_id} VTT → SRT 失敗，錯誤: {e}")
-
     def convert_srt_to_txt(self, video_id):
         srt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.srt")
         txt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.txt")
@@ -64,12 +49,43 @@ class DownloadCaptions(Step):
 
         try:
             # 讀取整個 SRT 檔
-            with open(srt_path, 'r', encoding='utf-8') as f:
+            with open(srt_path, 'r') as f:
                 srt_content = f.read()
-            with open(txt_path, 'w', encoding='utf-8') as txt_file:
+            with open(txt_path, 'w') as txt_file:
                 txt_file.write(srt_content)
 
             print(f"📄 已產生 {video_id}.txt")
             os.remove(srt_path)
         except Exception as e:
             print(f"轉檔 {video_id} SRT → TXT 失敗，錯誤: {e}")
+
+    def fix_youtube_vtt(self, video_id):
+        pretty_subtitle = ''
+        previous_caption_text = ''
+        vtt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.en.vtt")
+        srt_path = os.path.join(CAPTIONS_DIR, f"{video_id}.srt")
+        i = 1
+        for caption in webvtt.read(vtt_path):
+
+            if previous_caption_text == caption.text.strip():
+
+                converted_start = previous_caption_start.replace('.', ',')
+                converted_end = caption.end.strip().replace('.', ',')
+
+                pretty_subtitle += f"{i}\n{converted_start} --> {converted_end}\n{previous_caption_text}\n\n"
+
+                i += 1
+
+            elif previous_caption_text == caption.text.strip().split("\n")[0]:
+
+                previous_caption_text = caption.text.strip().split("\n")[1]
+                previous_caption_start = caption.start
+                last_caption_end = caption.end
+
+            else:
+                previous_caption_text = caption.text.strip()
+                previous_caption_start = caption.start.strip()
+
+        with open(srt_path, 'w') as srt_file:
+            srt_file.write(pretty_subtitle)
+        os.remove(vtt_path)
--- yt_concate/pipeline/steps/download_videos.py
+++ yt_concate/pipeline/steps/download_videos.py
@@ -1,5 +1,4 @@
 import os.path
-
 import yt_dlp
 from yt_concate.settings import VIDEOS_DIR
 
@@ -12,10 +11,17 @@ class DownloadVideos(Step):
         print(len(data))
         yt_set = set([found.yt for found in data])
         print('videos to download=', len(yt_set))
+
         ydl_opts = {
             'outtmpl': os.path.join(VIDEOS_DIR, '%(id)s.%(ext)s'),
+            'format': 'bestvideo[vcodec=h264][height<=480]+bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }],
             'download_archive': 'archive.txt'
         }
+
         for yt in yt_set:
             url = yt.url
             if utils.video_file_exists(yt):
